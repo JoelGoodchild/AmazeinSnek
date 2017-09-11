697891cd118fb3cbd804d93fa316eb939133aa70
JoelGoodchild <joel.goodchild@hotmail.com>
2017-09-11 10:56:52 +1000

    Changed camera angle and fixed player movement. -Joel

---
+++ Unity File/Assets/Roll.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roll : MonoBehaviour
+{
+    void Awake()
+    {
+
+    }
+
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 20.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 20.0f;
+
+        transform.Translate(-z, 0, 0);
+        transform.Translate(0, 0, x);
+    }
+}
